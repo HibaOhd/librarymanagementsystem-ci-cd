8497709dad783df4f9cecd5f5833e6b741ba2d0c
GHITA BELLAMINE <GHITA.bellamine@um6p.ma>
2025-12-05 23:58:51 +0100

    Update ApplicationTests.java

--- src/test/java/com/quickstartdev/librarymanagementsystem/ApplicationTests.java
+++ src/test/java/com/quickstartdev/librarymanagementsystem/ApplicationTests.java
@@ -3,11 +3,10 @@ package com.quickstartdev.librarymanagementsystem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = LibraryManagementSystemApplication.class)
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
