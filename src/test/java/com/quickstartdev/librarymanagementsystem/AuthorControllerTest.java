ca14fe9f448f47aa45b325eb00a6d0f892b4c514
Hiba OUHMAD <hibaouhmad12@gmail.com>
2025-12-06 02:33:28 +0100

    Create AuthorControllerTest.java

---
+++ src/test/java/com/quickstartdev/librarymanagementsystem/AuthorControllerTest.java
@@ -0,0 +1,49 @@
+package com.knf.dev.librarymanagementsystem;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import com.knf.dev.librarymanagementsystem.service.AuthorService;
+import com.knf.dev.librarymanagementsystem.entity.Author;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
+
+@WebMvcTest(controllers = AuthorController.class)
+class AuthorControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AuthorService authorService;
+
+    @Test
+    void testFindAuthorById_Existing() throws Exception {
+        Author a = new Author();
+        a.setId(1L);
+        a.setName("Some Author");
+        Mockito.when(authorService.findAuthorById(1L)).thenReturn(Optional.of(a));
+
+        mockMvc.perform(get("/author/1"))
+            .andExpect(status().isOk())
+            .andExpect(model().attributeExists("author"))
+            .andExpect(view().name("list-author"));
+    }
+
+    @Test
+    void testFindAllAuthors() throws Exception {
+
+        mockMvc.perform(get("/authors"))
+            .andExpect(status().isOk())
+            .andExpect(model().attributeExists("authors"))
+            .andExpect(view().name("list-authors"));
+    }
+}
